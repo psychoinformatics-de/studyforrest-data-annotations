2c7c42264b2980a72e4ae1f91b1e8aa65ebec95a
Christian Olaf Häusler <der.haeusler@gmx.net>
2018-02-15 17:15:03 +0100

    importer for speech_narrator.csv now working

--- code/importer/speech_narrator.py
+++ code/importer/speech_narrator.py
@@ -1,22 +1,29 @@
 #!/usr/bin/python
-
+'''
+'''
 from os.path import join as opj
 import pandas as pd
 
-df = pd.read_csv(opj('src', 'locations', 'data', 'structure.csv'))
+# read the annotation
+# TO DO: skiprows flagged with '???' here already?
+df = pd.read_csv(opj('src', 'voice', 'speech_narrator.csv'))
+
+# # drop rows with whole sentences as flagged with '???' in column 1
+df = df.loc[df.iloc[:,1] != '???']
+# reset index
+# df.index = range(0, len(df))
+
+# convert the cleaned columns to float64
+df.iloc[:, 0:2] = df.iloc[:, 0:2].astype('float64')
+
+# replace column 1 (end) with duration (end - start)
+df.iloc[:,1] = df.iloc[:,1] - df.iloc[:,0]
 
 # apply BIDS standard column names
-df.rename(columns=dict(time='onset'), inplace=True)
-
-# last line in input is not a shot, but the end of the last shot
-shot_durations = df['onset'].diff()[1:]
-shot_durations.index = range(0, len(shot_durations))
-df.drop(df.tail(1).index, inplace=True)
-# include BIDS standard duration column
-df.insert(1, 'duration', shot_durations)
+df.rename(columns=dict(start='onset', end='duration'), inplace=True)
 
 df.to_csv(
-    opj('researchcut', 'locations.tsv'),
+    opj('researchcut', 'speech_narrator.tsv'),
     index=False,
     sep='\t',
     float_format='%.3f')
